b4c2a6785504ad9a404e43a1ca87644067844de9
parkho79 <parkho79@gmail.com>
2019-03-11 09:24:17 +0900

    Implicit intent

--- app/src/main/java/com/parkho/intentsample/FirstActivity.java
+++ app/src/main/java/com/parkho/intentsample/FirstActivity.java
@@ -15,7 +15,7 @@ public class FirstActivity extends AppCompatActivity
         setContentView(R.layout.activity_first);
 
         // Explicit(명시적) intent 로 second activity 호출
-        OnClickListener clickListener = new OnClickListener() {
+        OnClickListener explicitClickListener = new OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplication(), SecondActivity.class);
@@ -30,6 +30,36 @@ public class FirstActivity extends AppCompatActivity
                 startActivity(intent);
             }
         };
-        findViewById(R.id.btn_explicit).setOnClickListener(clickListener);
+        findViewById(R.id.btn_explicit).setOnClickListener(explicitClickListener);
+
+        /*
+         * Implicit(암시적) intent 로 third activity 호출
+         *  - "com.parkho.action.Image_view" Action 으로 호출
+         */
+        OnClickListener implicitClickListener1 = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction("com.parkho.action.IMAGE_VIEW");
+                intent.addCategory("android.intent.category.EDIT");
+                startActivity(intent);
+            }
+        };
+        findViewById(R.id.btn_implicit_1).setOnClickListener(implicitClickListener1);
+
+        /*
+         * Implicit(암시적) intent 로 해당 action 을 처리할 수 있는 activity 호출
+         *  - URL 을 처리할 수 있는 브라우저 실행
+         */
+        OnClickListener implicitClickListener2 = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://parkho79.tistory.com"));
+                startActivity(intent);
+            }
+        };
+        findViewById(R.id.btn_implicit_2).setOnClickListener(implicitClickListener2);
     }
 }
